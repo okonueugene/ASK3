10459590a136a261836825e611452ace37bbc46f
okonueugene <tylarrex67@gmail.com>
2024-01-10 16:20:13 +0300

    guard login attendances()

--- app/Models/Guard.php
+++ app/Models/Guard.php
@@ -53,4 +53,29 @@ class Guard extends Authenticatable
         return $this->belongsTo(Site::class);
     }
 
+    public function patrols()
+    {
+        return $this->hasMany(Patrol::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function dobs()
+    {
+        return $this->hasMany(Dob::class);
+    }
+
+    public function incidents()
+    {
+        return $this->hasMany(Incident::class, 'guard_id');
+    }
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class, 'guard_id');
+    }
+
 }
