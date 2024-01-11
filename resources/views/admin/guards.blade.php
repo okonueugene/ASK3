c2713a7adf720a44b4fd8261b02b3aa062759709
okonueugene <tylarrex67@gmail.com>
2024-01-11 09:26:08 +0300

    css for active

--- resources/views/admin/guards.blade.php
+++ resources/views/admin/guards.blade.php
@@ -238,11 +238,11 @@
                                                     <span>{{ $guard->id_number }}</span>
                                                 </td>
                                                 <td class="nk-tb-col tb-col-lg">
-                                                    @if (!$guard->site)
-                                                        <span class="badge bg-danger">Not Assigned</span>
-                                                    @else
-                                                        <span>{{ $guard->site->name }}</span>
-                                                    @endif
+                                                    <? if($guard->site === null){ ?>
+                                                    <span class="badge bg-danger">Not Assigned</span>
+                                                    <? }else{ ?>
+                                                    <span>{{ $guard->site->name }}</span>
+                                                    <? } ?>
                                                 </td>
                                                 <td class="nk-tb-col tb-col-lg">
                                                     @if ($guard->is_active === 1)
