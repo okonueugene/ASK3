ed100105d3a4fe3131f7aa9c0823bdd8069d5979
okonueugene <tylarrex67@gmail.com>
2024-01-30 14:14:33 +0300

    retrieve all site tags

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Attendance;
+use App\Models\Guard;
+use App\Models\Site;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Activitylog\Models\Activity;
 
 class DashboardController extends Controller
 {
-    //
+
     public function index()
     {
-        return view('admin.dashboard');
+
+        $site = Site::where('company_id', Auth::user()->company->id)->get();
+
+        $coordinates = Site::where('company_id', Auth::user()->company->id)->whereNotNull('lat')->whereNotNull('long')->get();
+
+        $guards = Guard::where('company_id', Auth::user()->company->id)->get();
+
+        $present = Attendance::where('company_id', Auth::user()->company->id)->whereDate('created_at', Carbon::today())->get();
+        
+        $activities = Activity::whereDate('created_at', Carbon::today())->take(4)->get();
+
+        return view('admin.dashboard', compact('site', 'guards', 'present', 'coordinates', 'activities'));
     }
 }
--- resources/views/admin/commons/footer.blade.php
+++ resources/views/admin/commons/footer.blade.php
@@ -1,8 +1,7 @@
 <div class="nk-footer bg-white">
     <div class="container-fluid">
         <div class="nk-footer-wrap">
-            <div class="nk-footer-copyright"> &copy; 2023 Dashlite. Template by <a href="https://softnio.com"
-                    target="_blank">Softnio</a>
+            <div class="nk-footer-copyright"> &copy; {{ date("Y") }} Askari Technologies
             </div>
             <div class="nk-footer-links">
                 <ul class="nav nav-sm">
