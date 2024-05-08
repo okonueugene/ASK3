b2674c962fdc9983e92dceb1823a5486bf3e5ef9
okonueugene <tylarrex67@gmail.com>
2024-05-08 09:16:24 +0300

    invitations

---
+++ resources/views/auths/invalid-invitation.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+<head>
+    <base href="../../../">
+    <meta charset="utf-8">
+    <meta name="author" content="Softnio">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description"
+        content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="{{ asset('/images/favicon.png') }}">
+    <!-- Page Title  -->
+    <title>Askari Technologies</title>
+    <!-- StyleSheets  -->
+    <link rel="stylesheet" href="{{ asset('/assets/css/dashlite.css?ver=3.2.2') }}">
+    <link id="skin-default" rel="stylesheet" href="{{ asset('/assets/css/theme.css?ver=3.2.2') }}">
+</head>
+
+<body class="nk-body bg-white npc-general pg-auth">
+    <div class="nk-app-root">
+        <!-- main @s -->
+        <div class="nk-main ">
+            <!-- wrap @s -->
+            <div class="nk-wrap nk-wrap-nosidebar">
+                <!-- content @s -->
+                <div class="nk-content ">
+                    <div class="nk-block nk-block-middle nk-auth-body">
+                        <div class="brand-logo pb-5">
+                            <a href="html/index.html" class="logo-link">
+                                <img class="logo-light logo-img logo-img-lg" src="{{ asset('/images/site_logo.png') }}"
+                                    srcset="./images/logo2x.png 2x" alt="logo">
+                                <img class="logo-dark logo-img logo-img-lg" src="{{ asset('/images/site_logo.png') }}"
+                                    srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                            </a>
+                        </div>
+                        <div class="nk-block-head">
+                            <div class="nk-block-head-content">
+                                <h4 class="nk-block-title">Thank you for submitting form</h4>
+                                <div class="nk-block-des text-danger">
+                                    <p>However, the invitation link has expired. Please contact the administrator for a new invitation link.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="nk-footer nk-auth-footer-full">
+                        <div class="container wide-lg">
+                            <div class="row g-3">
+            
+                                <div class="col-lg-6">
+                                    <div class="nk-block-content text-center text-lg-left">
+                                        <p class="text-soft">&copy; {{ date('Y') }} Askari Technologies. All Rights Reserved.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- wrap @e -->
+            </div>
+            <!-- content @e -->
+        </div>
+        <!-- main @e -->
+    </div>
+    <!-- app-root @e -->
+    <!-- JavaScript -->
+    <script src="{{ asset('/assets/js/bundle.js?ver=3.2.2') }}"></script>
+    <script src="{{ asset('/assets/js/scripts.js?ver=3.2.2') }}"></script>
+
+
+</html>
