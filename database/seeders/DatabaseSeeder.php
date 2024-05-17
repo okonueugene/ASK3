4fca3681c7c09ba2f4db417c8ef2336e00c2fb92
okonueugene <tylarrex67@gmail.com>
2024-05-17 15:47:41 +0300

    clean db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,20 +19,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TimezoneSeeder::class,
 
-            // PermissionSeeder::class,
-            // RoleSeeder::class,
-            // PermissionRoleSeeder::class,
-            // RoleUserSeeder::class,
         ]);
-
-        \App\Models\User::factory(100)->create();
-        \App\Models\Site::factory(20)->create();
-        \App\Models\Guard::factory(200)->create();
-        \App\Models\Tag::factory(100)->create();
-        \App\Models\PatrolHistory::factory(1000)->create();
-        \App\Models\Patrol::factory(100)->create();
-        \App\Models\Incident::factory(100)->create();
-
 
     }
 }
