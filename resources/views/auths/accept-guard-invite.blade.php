f3aa0731aefc7eb17313000dab148b245ecbd04b
okonueugene <tylarrex67@gmail.com>
2024-05-29 11:03:13 +0300

    chore:add select2 #1

---
+++ resources/views/auths/accept-guard-invite.blade.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+<head>
+    <base href="../../../">
+    <meta charset="utf-8">
+    <meta name="author" content="Softnio">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="{{ asset('/images/favicon.png') }}">
+    <!-- Page Title  -->
+    <title>Register | Askari</title>
+    <!-- StyleSheets  -->
+    <link rel="stylesheet" href="{{ asset('/assets/css/dashlite.css?ver=3.2.2') }}">
+    <link id="skin-default" rel="stylesheet" href="{{ asset('/assets/css/theme.css?ver=3.2.2') }}">
+</head>
+
+<body class="nk-body bg-white npc-general pg-auth">
+    <div class="nk-app-root">
+        <!-- main @s -->
+        <div class="nk-main ">
+            <!-- wrap @s -->
+            <div class="nk-wrap nk-wrap-nosidebar">
+                <!-- content @s -->
+                <div class="nk-content ">
+                    <div class="nk-block nk-block-middle nk-auth-body wide-xs">
+                        <div class="brand-logo pb-4 text-center">
+                            <a href="html/index.html" class="logo-link">
+                                <img class="logo-light logo-img logo-img-lg" src="{{ asset('/images/site_logo.png') }}"
+                                    srcset="{{ asset('/images/logo2x.png 2x') }}" alt="logo">
+                                <img class="logo-dark logo-img logo-img-lg" src="{{ asset('/images/site_logo.png') }}"
+                                    srcset="{{ asset('/images/logo-dark2x.png 2x') }}" alt="logo-dark">
+                            </a>
+                        </div>
+                        <div class="card card-bordered">
+                            <div class="card-inner card-inner-lg">
+                                <div class="nk-block-head">
+                                    <div class="nk-block-head-content">
+                                        <h4 class="nk-block-title">Register</h4>
+                                        <div class="nk-block-des">
+                                            <p>Create New Askari Account</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <form action="{{ route('register-guard', ['token' => $token]) }}" method="POST">
+                                    @csrf
+                                    <input type="hidden" name="email" value="{{ $email }}">
+                                    <div class="form-group">
+                                        <label class="form-label" for="name">Name</label>
+                                        <div class="form-control-wrap">
+                                            <input type="text" class="form-control form-control-lg" id="name"
+                                                name="name" placeholder="Enter your name">
+                                        </div>
+                                        @error('name')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="email">Phone Number</label>
+                                        <div class="form-control-wrap">
+                                            <input type="text" class="form-control form-control-lg" id="phone"
+                                                name="phone" placeholder="Enter your phone number">
+
+                                        </div>
+                                        @error('phone')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                        @error('email')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="id_number">ID Number</label>
+                                        <div class="form-control-wrap">
+                                            <input type="text" class="form-control form-control-lg" id="id_number"
+                                                name="id_number" placeholder="Enter your ID number">
+                                        </div>
+                                        @error('id_number')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="password">Passcode</label>
+                                        <div class="form-control-wrap">
+                                            <a href="#" class="form-icon form-icon-right passcode-switch lg"
+                                                data-target="password">
+                                                <em class="passcode-icon icon-show icon ni ni-eye"></em>
+                                                <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
+                                            </a>
+                                            <input type="password" class="form-control form-control-lg" id="password"
+                                                name="password" placeholder="Enter your passcode">
+                                        </div>
+                                        @error('password')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="password">Confirm Passcode</label>
+                                        <div class="form-control-wrap">
+                                            <a href="#" class="form-icon form-icon-right passcode-switch lg"
+                                                data-target="password_confirmation">
+                                                <em class="passcode-icon icon-show icon ni ni-eye"></em>
+                                                <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
+                                            </a>
+                                            <input type="password" class="form-control form-control-lg"
+                                                id="password_confirmation" name="password_confirmation"
+                                                placeholder="Confirm your passcode">
+                                        </div>
+                                        @error('password_confirmation')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-control-xs custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="checkbox"
+                                                name="checkbox" required>
+                                            <label class="custom-control-label" for="checkbox">I agree to
+                                                Askari Technologies <a href="#">Privacy Policy</a> &amp; <a
+                                                    href="#">
+                                                    Terms.</a></label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button class="btn btn-lg btn-primary btn-block">Register</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="nk-footer nk-auth-footer-full">
+                        <div class="container wide-lg">
+                            <div class="row g-3">
+                                <div class="col-lg-6">
+                                    <div class="nk-block-content text-center text-lg-left">
+                                        <p class="text-soft">&copy; {{ date('Y') }} Askari. All Rights Reserved.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- wrap @e -->
+            </div>
+            <!-- content @e -->
+        </div>
+        <!-- main @e -->
+    </div>
+    <!-- app-root @e -->
+    <!-- JavaScript -->
+    <script src="{{ asset('/assets/js/bundle.js?ver=3.2.2') }}"></script>
+    <script src="{{ asset('/assets/js/scripts.js?ver=3.2.2') }}"></script>
+    <!-- select region modal -->
+    <script>
+        const checkbox = document.getElementById('checkbox');
+        const defaultMessage = checkbox.validationMessage; // Get default message
+
+        checkbox.setCustomValidity('Please agree to the terms and conditions'); // Set custom message
+        // Optionally, reset the message on checkbox change
+        checkbox.addEventListener('change', () => {
+            checkbox.setCustomValidity('');
+        });
+    </script>
+
+</html>
